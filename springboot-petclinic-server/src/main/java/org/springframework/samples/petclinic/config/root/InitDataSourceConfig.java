95207f85b41d08df49f5575c30aa4d4ba1894d59
mplescano <mplescano@gmail.com>
2016-11-03 20:29:48 -0500

    corrections

--- springboot-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/root/InitDataSourceConfig.java
+++ springboot-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/root/InitDataSourceConfig.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.petclinic.config.root;
 
+import java.io.IOException;
+
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
@@ -7,8 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.util.ResourceUtils;
 
 @Configuration
 public class InitDataSourceConfig {
@@ -19,11 +26,22 @@ public class InitDataSourceConfig {
 	@Autowired
 	private DataSource dataSource;
 	
+	@Autowired
+	private ResourcePatternResolver resourceLoader;
+	
 	@PostConstruct
-	public void init() {
+	public void init() throws IOException {
 		ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
-		databasePopulator.addScript(new ClassPathResource(env.getProperty("spring.datasource.schema")));
-		databasePopulator.addScript(new ClassPathResource(env.getProperty("spring.datasource.data")));
+		
+		Resource[] schemas = resourceLoader.getResources(env.getProperty("spring.datasource.schema"));
+		for (Resource schema : schemas) {
+			databasePopulator.addScript(schema);
+		}
+		
+		Resource[] datas = resourceLoader.getResources(env.getProperty("spring.datasource.data"));
+		for (Resource data : datas) {
+			databasePopulator.addScript(data);
+		}
 		DatabasePopulatorUtils.execute(databasePopulator, dataSource);
 	}
 
