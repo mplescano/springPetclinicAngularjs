d0a35689e12b3344ad207508ddd831ebab4c7416
Mack Lescano <mack.lescano@lemonade.com>
2024-10-14 16:04:44 -0500

    updated files

--- springboot-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/servlet/MvcViewConfig.java
+++ springboot-petclinic-server/src/main/java/org/springframework/samples/petclinic/config/servlet/MvcViewConfig.java
@@ -4,17 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 @Configuration
 public class MvcViewConfig {
 
 	@Autowired
 	private Environment env;
-	
-	@Bean 
+
+	@Bean
 	public SpringResourceTemplateResolver templateResolver() {
 		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
 		resolver.setPrefix(env.getProperty("spring.thymeleaf.prefix"));
@@ -23,19 +23,19 @@ public class MvcViewConfig {
 		resolver.setOrder(1);
 		return resolver;
 	}
-	
-	@Bean 
+
+	@Bean
 	public SpringTemplateEngine templateEngine() {
 		SpringTemplateEngine engine = new SpringTemplateEngine();
 		engine.setTemplateResolver(templateResolver());
 		return engine;
 	}
-	
-	@Bean 
+
+	@Bean
 	public ThymeleafViewResolver thymeleafViewResolver() {
 		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
 		resolver.setTemplateEngine(templateEngine());
 		return resolver;
 	}
-	
+
 }
